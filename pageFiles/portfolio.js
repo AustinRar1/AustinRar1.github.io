960a30675939902bc83844eaaf99abf1c3f2bf44
Austin Rardin <ajeflaunch@gmail.com>
2018-05-31 09:58:37 -0600

    Portfolio.finish

--- pageFiles/portfolio.js
+++ pageFiles/portfolio.js
@@ -1,23 +1,51 @@
-//nav function 
 function openNav() {
-    document.getElementById("menuIcon").style.width = "100%";
+    document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("menuIcon").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%";
 }
-//modal contact form
-var modal = document.getElementById('mod_Content');
-var btn = document.getElementById('modButton');
-var span =document.getElementsByClassName('close')[0]
-btn.onclick=function(){
+// nav menu open and close
+
+// modal
+let modal = document.getElementById('contact-Modal');
+
+let modalBtn = document.getElementById('modButton');
+
+let closeBtn= document.getElementsByClassName('closeButn')[0];
+
+window.addEventListener('click', clickOutside)
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+function openModal(){
     modal.style.display = 'block';
 }
-span.onclick = function() {
-    modal.style.display = "none";
+
+function closeModal(){
+    modal.style.display = 'none';
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+
+
+function clickOutside(e){
+if(e.target==modal)
+    modal.style.display = 'none';
+}
+// modal
+
+//interests tabs 
+
+function openCity(evt, interests) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(interests).style.display = "block";
+    evt.currentTarget.className += " active";
 }
+document.getElementById("defaultOpen").click();
+
